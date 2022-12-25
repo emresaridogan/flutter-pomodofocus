25ccf76861355d90b8067462482ddf7459aa9496
Emresaridogan <emresaridogan@outlook.com>
2022-12-25 19:57:21 +0300

    base state stl added

---
+++ lib/core/base/state/base_state_stl.dart
@@ -0,0 +1,15 @@
+import 'package:pomodofocus/core/constants/app/app_constants.dart';
+import 'package:pomodofocus/core/constants/color/color_constants.dart';
+import 'package:pomodofocus/core/constants/image/image_constants.dart';
+import 'package:pomodofocus/core/constants/padding/padding_constants.dart';
+import 'package:pomodofocus/core/constants/radius/radius_constants.dart';
+import 'package:pomodofocus/core/constants/string/string_constants.dart';
+
+abstract class BaseStateStl {
+  ColorConstants colorConstants = ColorConstants.instance;
+  ImageConstants imageConstants = ImageConstants.instance;
+  ApplicationConstants applicationConstants = ApplicationConstants.instance;
+  PaddingConstants paddingConstants = PaddingConstants.instance;
+  RadiusConstants radiusConstants = RadiusConstants.instance;
+  StringConstants stringConstants = StringConstants.instance;
+}
