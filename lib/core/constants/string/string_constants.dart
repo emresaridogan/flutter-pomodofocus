d8f7683ea7abfd8810cdb6824de361cbde624379
Emresaridogan <emresaridogan@outlook.com>
2022-12-26 18:49:24 +0300

    constants updated

--- lib/core/constants/string/string_constants.dart
+++ lib/core/constants/string/string_constants.dart
@@ -12,14 +12,21 @@ class StringConstants {
   get onboard3button => "Get Started";
   get onboard3 => "Boost your productivity now & be successful";
 
+  // account setup
+  get AccountSetupTitle => "Let's you in";
+  get continueFacebook => "Continue with Facebook";
+  get continueGoogle => "Continue with Google";
+  get continueApple => "Continue with Apple";
+  get or => "or";
+  get signInPass => "Sign in with password";
   // login
   get login => "Login to your Account";
   get email => "Email";
   get password => "Password";
   get remember => "Remember me";
   get signin => "Sign in";
-  get or => "or continue with";
-  get dontHave => "Don't have account?";
+  get orWith => "or continue with";
+  get dontHave => "Don't have an account?";
   get forgotPass => "Forgot the password?";
 
   //register
