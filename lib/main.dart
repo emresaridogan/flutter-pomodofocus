561e083d83ce388ec09eb845aec7193636fd951c
Emresaridogan <emresaridogan@outlook.com>
2022-12-30 14:56:52 +0300

    project updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:pomodofocus/core/constants/app/app_constants.dart';
-import 'package:pomodofocus/view/onboard/splash_view.dart';
+import 'package:pomodofocus/core/base/state/base_state_stl.dart';
+import 'package:pomodofocus/view/splash/splash_view.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatelessWidget with BaseStateStl {
+  MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: ApplicationConstants.instance.appName,
+      title: applicationConstants.appName,
       theme: ThemeData(
         primarySwatch: Colors.teal,
       ),
