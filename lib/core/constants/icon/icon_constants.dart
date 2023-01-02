0bb619cb2be8ea7e8680fd4f3bdb9fee0b870a20
Emresaridogan <emresaridogan@outlook.com>
2023-01-02 22:37:47 +0300

    project updated

--- lib/core/constants/icon/icon_constants.dart
+++ lib/core/constants/icon/icon_constants.dart
@@ -8,4 +8,6 @@ class IconConstants {
   IconConstants._init();
 
   Icon get email => const Icon(Icons.email);
+  Icon get lock => const Icon(Icons.lock);
+  Icon get passwordHide => const Icon(Icons.remove_red_eye);
 }
