3e9a8a8ac89bf758a00b2b7fc5f5fb37409fac77
Emresaridogan <emresaridogan@outlook.com>
2023-01-19 18:42:34 +0300

    toast message creator added

--- lib/core/constants/app/app_constants.dart
+++ lib/core/constants/app/app_constants.dart
@@ -1,3 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
 class ApplicationConstants {
   static ApplicationConstants? _instance;
 
@@ -10,4 +13,15 @@ class ApplicationConstants {
 
   String get assetPath => 'assets/';
   String get imagesPath => '${assetPath}images/';
+}
+
+void toast(String message) {
+  Fluttertoast.showToast(
+      msg: message,
+      toastLength: Toast.LENGTH_SHORT,
+      gravity: ToastGravity.BOTTOM,
+      timeInSecForIosWeb: 1,
+      backgroundColor: Colors.grey.shade500,
+      textColor: Colors.black,
+      fontSize: 16.0);
 }
