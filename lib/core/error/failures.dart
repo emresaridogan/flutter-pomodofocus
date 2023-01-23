528a2280b4e6ed9866c423068e7bc2b6a765deef
Emresaridogan <emresaridogan@outlook.com>
2023-01-23 14:41:39 +0300

    error created

---
+++ lib/core/error/failures.dart
@@ -0,0 +1,15 @@
+import 'package:equatable/equatable.dart';
+
+abstract class Failure extends Equatable {
+  @override
+  List<Object> get props => [];
+}
+
+// General failures
+class ServerFailure extends Failure {}
+
+class CacheFailure extends Failure {}
+
+class InvalidInputFailure extends Failure {}
+
+class AuthenticationFailure extends Failure {}
