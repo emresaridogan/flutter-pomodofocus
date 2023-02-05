a9af86c0a95c8879d729ad25dc2df5fc9920260f
emresaridogan <emresaridogan@outlook.com>
2023-02-05 23:28:39 +0300

    task event created

---
+++ lib/features/presentation/bloc/task/task_event.dart
@@ -0,0 +1,52 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+part of 'task_bloc.dart';
+
+abstract class TaskEvent extends Equatable {}
+
+class CreateTaskButtonPressed extends TaskEvent {
+  String title;
+  String id;
+  String uid;
+  String date;
+  String time;
+  String category;
+  int countLongBreak;
+  int countSession;
+  int countShortBreak;
+  CreateTaskButtonPressed({
+    required this.id,
+    required this.title,
+    required this.uid,
+    required this.date,
+    required this.time,
+    required this.category,
+    required this.countLongBreak,
+    required this.countSession,
+    required this.countShortBreak,
+  });
+
+  @override
+  List<Object?> get props => [
+        id,
+        title,
+        uid,
+        date,
+        time,
+        category,
+        countLongBreak,
+        countSession,
+        countShortBreak,
+      ];
+}
+
+class GetAllTask extends TaskEvent {
+  String userId;
+  String date;
+  GetAllTask({
+    required this.userId,
+    required this.date,
+  });
+
+  @override
+  List<Object?> get props => [userId, date];
+}
