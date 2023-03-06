9291570f36d0e6b244e470245eb7a1ebb6eb1bf9
emresaridogan <yesaridogan@gmail.com>
2023-03-06 20:58:24 +0300

    home view updated

--- lib/features/presentation/view/home/home_view.dart
+++ lib/features/presentation/view/home/home_view.dart
@@ -1,15 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:percent_indicator/circular_percent_indicator.dart';
 import 'package:pomodofocus/core/base/state/base_state.dart';
-import 'package:pomodofocus/core/base/view/base_view.dart';
-import 'package:pomodofocus/features/presentation/bloc/auth/authentication_bloc.dart';
-import 'package:pomodofocus/features/presentation/bloc/task/task_bloc.dart';
-
-import '../../widget/home/single_task_card_widget.dart';
-import '../../../../core/components/date/date_picker.dart';
-import '../../bloc/auth/authentication_event.dart';
-import '../../bloc/auth/authentication_state.dart';
-import '../../widget/home/month_year_title.dart';
+import '../../../../core/base/view/base_view.dart';
+import '../../bloc/task/task_bloc.dart';
+import '../../widget/task/taskCardButton.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
@@ -19,68 +15,124 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends BaseState<HomeView> {
+  late String uid;
   late DateTime selectedDay;
-  late String uid, day, month, year;
+
   @override
   void initState() {
-    BlocProvider.of<AuthenticationBloc>(context).add(PageStarted());
     selectedDay = DateTime.now();
-
+    BlocProvider.of<TaskBloc>(context).add(GetAllTask(
+        date:
+            "${selectedDay.year.toString()}-${selectedDay.month.toString().padLeft(2, '0')}-${selectedDay.day.toString().padLeft(2, '0')}",
+        userId: "O5TbWBjD8bOs58wh1JkuICvrCwt1"));
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return BlocListener<AuthenticationBloc, AuthenticationState>(
-        listener: ((context, state) {
-          if (state is Authenticated) {
-            uid = state.uid;
-            BlocProvider.of<TaskBloc>(context).add(GetAllTask(
-                date:
-                    "${selectedDay.day.toString().padLeft(2, '0')}.${selectedDay.month.toString().padLeft(2, '0')}.${selectedDay.year.toString()}",
-                userId: uid));
-          }
-        }),
-        child: Scaffold(
-            backgroundColor: colorConstants.whiteColor,
-            appBar: AppBar(
-              backgroundColor: Colors.transparent,
-              elevation: 0,
-              title: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Image.asset(
-                    imageConstants.logo,
-                    fit: BoxFit.cover,
-                    scale: 4,
-                  ),
-                  Text(
-                    'Home',
-                    style: TextStyle(
-                        color: colorConstants.blackColor,
-                        fontSize: 25,
-                        fontWeight: FontWeight.bold),
-                  ),
-                  Icon(
-                    Icons.search,
-                    color: colorConstants.blackColor,
-                  )
-                ],
-              ),
-            ),
-            body: BaseView(
-              onPageBuilder: (context) => SingleChildScrollView(
-                  physics: const ScrollPhysics(), child: column),
-            )));
+    return BlocBuilder<TaskBloc, TaskState>(
+      builder: (context, state) {
+        if (state is TaskLoading) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        } else if (state is TaskLoaded) {
+          return Scaffold(
+              appBar: appBarField(),
+              body: BaseView(
+                onPageBuilder: (context) => getBody(context),
+              ));
+        }
+        return const SizedBox();
+      },
+    );
   }
 
-  Widget get column => Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        mainAxisSize: MainAxisSize.min,
+  AppBar appBarField() {
+    return AppBar(
+      backgroundColor: colorConstants.whiteColor,
+      elevation: 0,
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          MonthYearTitle(selectedDay: selectedDay),
-          datePicker(context),
-          BlocBuilder<TaskBloc, TaskState>(builder: ((context, state) {
+          Image.asset(
+            imageConstants.logo,
+            fit: BoxFit.cover,
+            scale: 4,
+          ),
+          Text(
+            'Pomodofocus',
+            style: GoogleFonts.poppins(
+              textStyle: Theme.of(context).textTheme.titleLarge!.copyWith(
+                  color: colorConstants.blackColor,
+                  fontWeight: FontWeight.w600),
+              fontSize: 20,
+            ),
+          ),
+          Icon(
+            Icons.search,
+            color: colorConstants.blackColor,
+          )
+        ],
+      ),
+    );
+  }
+
+  Column getBody(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Expanded(
+          flex: 15,
+          child: Column(
+            children: [
+              chartField(context),
+              SizedBox(
+                height: dynamicHeight(0.02),
+              ),
+              Expanded(
+                flex: 6,
+                child: textsField(context),
+              )
+            ],
+          ),
+        )
+      ],
+    );
+  }
+
+  Column textsField(BuildContext context) {
+    return Column(
+      children: [
+        Expanded(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                "Today Tasks",
+                style: GoogleFonts.poppins(
+                  textStyle: Theme.of(context).textTheme.titleLarge!.copyWith(
+                      color: colorConstants.blackColor,
+                      fontWeight: FontWeight.w600),
+                  fontSize: 18,
+                ),
+              ),
+              Text(
+                "See all",
+                style: GoogleFonts.poppins(
+                  textStyle: Theme.of(context).textTheme.titleLarge!.copyWith(
+                        color: colorConstants.secondaryColor,
+                        fontWeight: FontWeight.w500,
+                      ),
+                  fontSize: 18,
+                ),
+              ),
+            ],
+          ),
+        ),
+        Expanded(
+          flex: 10,
+          child: BlocBuilder<TaskBloc, TaskState>(builder: ((context, state) {
             if (state is TaskLoading) {
               return const Center(
                 child: CircularProgressIndicator(),
@@ -88,23 +140,77 @@ class _HomeViewState extends BaseState<HomeView> {
             } else if (state is TaskLoaded) {
               return state.tasks.isEmpty
                   ? _noTasksYetWidget()
-                  : ListView.builder(
-                      physics: const NeverScrollableScrollPhysics(),
-                      shrinkWrap: true,
-                      itemCount: state.tasks.length,
-                      itemBuilder: (context, index) {
-                        final task = state.tasks[index];
-                        return Padding(
-                          padding: EdgeInsets.only(bottom: dynamicHeight(0.01)),
-                          child: SingleTaskCardWidget(task: task),
-                        );
-                      },
-                    );
+                  : taskBuilder(state);
             }
             return _noTasksYetWidget();
-          }))
+          })),
+        ),
+      ],
+    );
+  }
+
+  ListView taskBuilder(TaskLoaded state) {
+    return ListView.builder(
+      physics: const NeverScrollableScrollPhysics(),
+      shrinkWrap: true,
+      itemCount: state.tasks.length,
+      itemBuilder: (context, index) {
+        final task = state.tasks[index];
+        return Padding(
+          padding: EdgeInsets.only(bottom: dynamicHeight(0.01)),
+          child: TaskCardButton(
+            title: task.title,
+            subtitle: task.category,
+            color: colorConstants.primaryColor,
+            textColor: colorConstants.whiteColor,
+          ),
+        );
+      },
+    );
+  }
+
+  Expanded chartField(BuildContext context) {
+    return Expanded(
+      flex: 2,
+      child: Row(
+        children: [
+          Expanded(
+            flex: 2,
+            child: chartIndicatorField(),
+          ),
+          SizedBox(
+            width: dynamicWidth(0.03),
+          ),
+          Expanded(flex: 3, child: chartTextField(context)),
         ],
-      );
+      ),
+    );
+  }
+
+  Text chartTextField(BuildContext context) {
+    return Text(
+      "Wow! Your daily goals is almost done!",
+      style: GoogleFonts.poppins(
+        textStyle: Theme.of(context).textTheme.titleLarge!.copyWith(
+            color: colorConstants.blackColor, fontWeight: FontWeight.w600),
+        fontSize: 18,
+      ),
+    );
+  }
+
+  CircularPercentIndicator chartIndicatorField() {
+    return CircularPercentIndicator(
+      progressColor: colorConstants.secondaryColor,
+      radius: 60.0,
+      lineWidth: 13.0,
+      animation: true,
+      percent: 0.75,
+      center: const Text(
+        "75%",
+        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
+      ),
+    );
+  }
 
   Column _noTasksYetWidget() {
     return Column(
@@ -128,31 +234,6 @@ class _HomeViewState extends BaseState<HomeView> {
           ],
         ),
       ],
-    );
-  }
-
-  Widget datePicker(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.only(top: 5),
-      child: DatePicker(
-          selectedDay: selectedDay,
-          changeDay: (value) {
-            setState(() {
-              selectedDay = value;
-            });
-
-            BlocProvider.of<TaskBloc>(context).add(GetAllTask(
-                date:
-                    "${selectedDay.day.toString().padLeft(2, '0')}.${selectedDay.month.toString().padLeft(2, '0')}.${selectedDay.year.toString()}",
-                userId: uid));
-          },
-          enableWeeknumberText: false,
-          backgroundColor: Colors.white,
-          digitsColor: Colors.black,
-          selectedBackgroundColor: colorConstants.secondaryColor,
-          selectedDigitColor: Colors.blue,
-          weekdays: const ['M', 'T', 'W', 'T', 'F', 'S', 'S'],
-          daysInWeek: 7),
     );
   }
 }
